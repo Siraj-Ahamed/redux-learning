3dc5c3b55595c33138aa88f3a413141538aa327e
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-03-08 07:40:53 +0530

    Installing react bootstrap and bootstrap icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <h1>Project Managment</h1>
+            <p>Currently 0 task(s) pending</p>
+        </>
+    );
+};
+
+export default Navbar;
